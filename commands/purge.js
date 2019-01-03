47c20a21ac8fe306b61e28984dc7a5cead5439a7
Basil Thaddeus <basiltqsi@gmail.com>
2019-01-03 21:53:56 +0400

    Added Purge Command
    Deletes messages

---
+++ commands/purge.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+let prefix = require("../configs/botConfig.json").prefix;
+
+module.exports.run = async(bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("you do not have permission to manage user messages.");
+    if(!args[0] || isNaN(args[0])) return message.reply(`please use the command format: \`${prefix}purge [Number]\``);
+    if(args[0] === "0") return message.reply("to delete messages, you need to have a value greater then 0.");
+    let mNum = Math.ceil(args[0]);
+    if(mNum === 1) mNum = 2;
+
+    let delEmbed = new Discord.RichEmbed()
+        .setTitle("🗑 Messages Deleted")
+        .setColor("#9f00ff")
+        .setDescription(`Deleted ${mNum} messages.`)
+        .setFooter(message.guild.name);
+
+    message.delete().then(message.channel.bulkDelete(mNum).then(() => {
+        message.channel.send(delEmbed).then(msg => msg.delete(5000));
+    }));
+};
+
+module.exports.help = {
+    name: "purge",
+    aliases: ["prune", "delete"]
+};
+
+/*
+
+    purge [Amount]
+
+ */

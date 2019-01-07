ce5824fddf8be9ab7df038f6bb56d3c25b4f5518
Basil Thaddeus <36113366+BasilThaddeus@users.noreply.github.com>
2019-01-07 11:45:52 +0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,11 +2,10 @@ const Discord = require("discord.js");
 
 module.exports = async(bot) => {
     console.log(`[BOT] Version 1.0.0\n[BOT] Made by BasilT\n[BOT] Running on ${bot.guilds.size} servers\n[BOT] Ready`);
-
-    let status = ["!help", `on ${bot.guilds.size} servers`];
     let i = 0;
 
     setInterval(function(){
+        let status = ["!help", `on ${bot.guilds.size} servers`];
         bot.user.setActivity(status[i]);
         i++;
         if(i === status.length) i = 0;

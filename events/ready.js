4d1b395f87de5a6eed1c9aa4014e34ec52da4606
Basil Thaddeus <36113366+BasilThaddeus@users.noreply.github.com>
2019-01-15 20:12:14 +0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ module.exports = async(bot) => {
 
     let i = 0;
     setInterval(function(){
-        let status = ["!help", `on ${bot.guilds.size} servers`];
+        let status = ["shutting down in 1 day for redesign."];
         bot.user.setActivity(status[i]);
         i++;
         if(i === status.length) i = 0;
